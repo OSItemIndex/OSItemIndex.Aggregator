412649df2b5b976701ebd7f2b4d8422563f5d825
Twinki <thetwinkehh@gmail.com>
2021-07-13 12:38:34 -0500

    wip: add events

---
+++ src/OSItemIndex.Aggregator/Services/Interfaces/IRealtimePriceService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using OSItemIndex.Data;
+
+namespace OSItemIndex.Aggregator.Services
+{
+    public interface IRealtimePriceService : IStatefulService
+    {
+        Task AggregateAsync<T>(string uri, EventSource source) where T: PriceEntity;
+    }
+}
